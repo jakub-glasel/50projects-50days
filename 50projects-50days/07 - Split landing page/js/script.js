194ecce4c0557eba36180d08dd8dfcf55bdcb84c
jglasel <jakub.glasel@888holdings.com>
2021-03-26 09:40:16 +0000

    changed folder names and js links accordingly

--- 50projects-50days/07 - Split landing page/js/script.js
+++ 50projects-50days/07 - Split landing page/js/script.js
@@ -1,13 +1,18 @@
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const container = document.querySelector('.container');
+const title = document.querySelector('#title');
 
 left.addEventListener('mousemove', () => container.classList.add('hover-left'))
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+left.addEventListener('mousemove', () => title.style.color = 'black')
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
 
 
 
 right.addEventListener('mousemove', () => container.classList.add('hover-right'))
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
-
+right.addEventListener('mousemove', () => title.style.color = 'white')
 
