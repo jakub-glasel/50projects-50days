4c353fa020c57957bc7a4a441baadfbd5975ce55
jglasel <jakub.glasel@888holdings.com>
2021-03-30 10:53:40 +0100

    day11 project finish

---
+++ 50projects-50days/11 - Key codes/js/script.js
@@ -0,0 +1,23 @@
+const insert = document.querySelector('.insert')
+
+
+
+window.addEventListener('keydown', (event) => {
+    insert.innerHTML = `
+  <div class="key">
+${event.key === ' ' ? 'Space' : event.key}  
+        <small></small>
+        </div>
+
+        <div class="key">
+        ${event.keyCode}
+        <small>event.keyCode</small>
+        </div>
+        <div class="key">
+        ${event.code}
+        <small>event.code</small>
+        </div>`
+
+
+
+})
