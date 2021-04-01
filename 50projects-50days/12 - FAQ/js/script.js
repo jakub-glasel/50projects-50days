fb0b5f4352d4e7618ee058ebabf9cf0679bd1968
Jakub Glasel <jakub.glasel@gmail.com>
2021-04-01 14:33:35 +0100

    day12 project done

---
+++ 50projects-50days/12 - FAQ/js/script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll('.faq-toggle')
+
+buttons.forEach(toggle => {
+    toggle.addEventListener('click', () =>
+        toggle.parentNode.classList.toggle('active'))
+})
+
